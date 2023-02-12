c9528684ea76e851ab8468d29c9dd85248188e12
Joseph Murray <josephwmurray@gmail.com>
2023-02-12 15:54:55 -0800

    accomplish TDD item 3, numbering object list items

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 function TaskList() {
   this.tasks = {};
+  this.taskNumber = 0;
 }
 
-function Tasks(task) {
-  this.task = task;
+TaskList.prototype.assignNumber = function() {
+  this.taskNumber += 1;
+  return this.taskNumber;
+}
+
+TaskList.prototype.addTask = function(task) {
+  task.taskNumber = this.assignNumber();
+  this.tasks[task.chore] = task;
+}
+
+function Tasks(chore) {
+  this.chore = chore;
 }
--- readme.md
+++ readme.md
@@ -2,3 +2,10 @@ test: create an object containing a list of tasks with IDs
 code: input["do dishes", "make bed"]
 outcome: do dishes, make bed
 
+test: write a function to be able to add new tasks
+code: input["wash car"]
+outcome: do dishes, make bed, wash car
+
+test: write a function to number the tasks
+code: taskList;
+outcome: ["wash car", task 1]
